be24dbe7413dfcab4a49c3d02195baa77bd31bb5
Rubesh <rubeshkk28@gmail.com>
2023-09-17 05:32:20 +0530

    Update Hack.py

--- Hack.py
+++ Hack.py
@@ -24,11 +24,6 @@ audio_file = st.file_uploader("Upload an audio file (in WAV format)", type=["wav
 if audio_file is not None:
     st.audio(audio_file, format='audio/wav')
 
-    # Extract MFCC features and make a prediction
-    temp_audio_file = tempfile.NamedTemporaryFile(delete=False, suffix=".wav")
-    temp_audio_file.write(audio_file.read())
-    temp_audio_file.close()
-
     mfcc_features = extract_mfcc_for_prediction(temp_audio_file.name)
     X_pred = np.expand_dims(mfcc_features, axis=0)
     X_pred = np.expand_dims(X_pred, axis=-1)
@@ -43,12 +38,3 @@ if audio_file is not None:
 
     st.subheader("Predicted Emotion:")
     st.write(predicted_emotion)
-
-    # Display the audio waveform
-    audio_data, sampling_rate = librosa.load(temp_audio_file.name, sr=None)
-    librosa.display.waveshow(audio_data, sr=sampling_rate, color='b')
-    st.pyplot(fig=None, clear_figure=None, use_container_width=True, **kwargs)
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-
-    # Remove the temporary audio file
-    os.unlink(temp_audio_file.name)
